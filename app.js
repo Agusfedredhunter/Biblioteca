eb653a4f9ecdf821abef7078ac5d5885b6bc6613
joacopelizza <joaquinpelizzayt@gmail.com>
2026-05-12 20:44:13 -0300

    agregar archivos estáticos

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 
 const serviciosRouter = require('./routes/servicios');
@@ -12,15 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname)));
-
 app.use('/servicios', serviciosRouter);
 app.use('/equipo', equipoRouter);
 app.use('/perfil', usuariosRouter);
 app.use('/login', usuariosRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.json({ mensaje: 'API Helenic Works funcionando correctamente' });
 });
 
 module.exports = app;
